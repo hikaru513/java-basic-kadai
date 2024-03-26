f632cd53e1f3ba8411399a29b11ca70b9fc6bfc5
hikaru513 <138415205+hikaru513@users.noreply.github.com>
2024-03-26 15:51:45 +1000

    Create CarExec_Chapter14.java

---
+++ kadai_014/CarExec_Chapter14.java
@@ -0,0 +1,14 @@
+package kadai_014;
+
+public class CarExec_Chapter14 {
+
+	public static void main(String[] args) {
+		// インスタンス化
+        Car_Chapter14 gear = new Car_Chapter14();
+
+        // ギアを変更して、結果を出力
+        gear.gearChange(3);
+        gear.run();
+	}
+
+}
