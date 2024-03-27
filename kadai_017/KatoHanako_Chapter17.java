a4af23f4f55d2f2885cb1b28bd84c70c180ff18a
hikaru513 <138415205+hikaru513@users.noreply.github.com>
2024-03-27 11:19:20 +1000

    Create KatoHanako_Chapter17.java

---
+++ kadai_017/KatoHanako_Chapter17.java
@@ -0,0 +1,18 @@
+package kadai_017;
+
+public class KatoHanako_Chapter17 extends Kato_Chapter17{
+	
+
+	// コンストラクタで名前を設定
+    public KatoHanako_Chapter17() {
+        this.givenName = "花子";
+    }
+	
+	
+	   
+	   //抽象メソッドに具体的な処理を記述	する
+	   void eachIntroduce() {
+		System.out.println("好きな食べ物はリンゴです");
+	   }
+	
+}
