feea27dbf1fea642d1276f75d00d74e82a7f1962
hikaru513 <138415205+hikaru513@users.noreply.github.com>
2024-03-27 20:11:50 +1000

    Update KatoTaro_Chapter17.java

--- kadai_017/KatoTaro_Chapter17.java
+++ kadai_017/KatoTaro_Chapter17.java
@@ -1,20 +1,18 @@
 package kadai_017;
 
 public class KatoTaro_Chapter17 extends Kato_Chapter17{
-	
-	public void setGivenName(String givenName) {
-        this.givenName = givenName;
+    
+   
+    public void setGivenName() {
+        this.givenName = "太郎";  
     }
-	// コンストラクタの追加
+    // コンストラクタの追加
     public KatoTaro_Chapter17() {
-        setGivenName("太郎"); // ここで名前をセット
+        setGivenName(); 
     }
-	
 
-	   //抽象メソッドに具体的な処理を記述	する
-	   void eachIntroduce() {
-	      System.out.println("私はJavaが得意です");
-	   }
-	
+    // 抽象メソッドに具体的な処理を記述する
+    void eachIntroduce() {
+        System.out.println("私はJavaが得意です");
+    }
 }
-
