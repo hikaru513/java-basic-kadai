96728c735b2aba68dbf0420661a17b1c67b7e5d1
hikaru513 <138415205+hikaru513@users.noreply.github.com>
2024-03-27 11:37:07 +1000

    Update KatoHanako_Chapter17.java

--- kadai_017/KatoHanako_Chapter17.java
+++ kadai_017/KatoHanako_Chapter17.java
@@ -2,13 +2,13 @@ package kadai_017;
 
 public class KatoHanako_Chapter17 extends Kato_Chapter17{
 	
-
+	public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
 	// コンストラクタで名前を設定
     public KatoHanako_Chapter17() {
-        this.givenName = "花子";
+    	setGivenName( "花子");
     }
-	
-	
 	   
 	   //抽象メソッドに具体的な処理を記述	する
 	   void eachIntroduce() {
