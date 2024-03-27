ff82c3f0846617e994e1385d3ccf68df43e01db0
hikaru513 <138415205+hikaru513@users.noreply.github.com>
2024-03-27 11:36:39 +1000

    Update KatoIchiro_Chapter17.java

--- kadai_017/KatoIchiro_Chapter17.java
+++ kadai_017/KatoIchiro_Chapter17.java
@@ -2,16 +2,17 @@ package kadai_017;
 
 public class KatoIchiro_Chapter17 extends Kato_Chapter17{
 	
-
+	public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
 	 // コンストラクタで名前を設定
     public KatoIchiro_Chapter17() {
-        this.givenName = "一郎";
+    	setGivenName( "一郎");
     }
 	
-	
-	   
 	   //抽象メソッドに具体的な処理を記述	する
 	   void eachIntroduce() {
 	      System.out.println("好きな食べ物はリンゴです");
 	   }
 }
+
