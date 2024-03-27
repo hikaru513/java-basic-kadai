830fed581f9e77eefd4c81954b5f4d35ab3cd851
hikaru513 <138415205+hikaru513@users.noreply.github.com>
2024-03-27 11:20:19 +1000

    Update Kato_Chapter17.java

--- kadai_017/Kato_Chapter17.java
+++ kadai_017/Kato_Chapter17.java
@@ -3,13 +3,9 @@ package kadai_017;
 abstract public class Kato_Chapter17 {
 
 	// フィールド（内部データ）
-    private String familyName  = "加藤"; // 
-    public String givenName;  // 
-    private String address = "東京都中野区〇×" ;  // 住所
-    
-    public void setGivenName(String givenName) {
-        this.givenName = givenName;
-    }
+    private String familyName  = "加藤"; 
+    public String givenName;  
+    private String address = "東京都中野区〇×" ;  
 
     
     public void commonIntroduce() {
