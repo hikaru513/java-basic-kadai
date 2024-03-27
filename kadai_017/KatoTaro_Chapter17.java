9d813f204d4439cc03f5c9e106ca6bb26914685d
hikaru513 <138415205+hikaru513@users.noreply.github.com>
2024-03-27 11:18:12 +1000

    Create KatoTaro_Chapter17.java

---
+++ kadai_017/KatoTaro_Chapter17.java
@@ -0,0 +1,17 @@
+package kadai_017;
+
+public class KatoTaro_Chapter17 extends Kato_Chapter17{
+	
+	 // コンストラクタで名前を設定
+    public KatoTaro_Chapter17() {
+        this.givenName = "太郎";
+    }
+	
+
+	   //抽象メソッドに具体的な処理を記述	する
+	   void eachIntroduce() {
+	      System.out.println("私はJavaが得意です");
+	   }
+	
+
+}
